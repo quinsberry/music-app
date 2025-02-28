9c8bbc17faa71c560623fe8ae4b3f48a86c71a54
ekolesnyk <eugene.kolesnyk@customertimes.com>
2025-02-28 02:31:11 +0100

    fix(apps/api): fixed pagination

--- apps/api/src/modules/song/song.controller.ts
+++ apps/api/src/modules/song/song.controller.ts
@@ -9,23 +9,35 @@ export class SongController {
     @Get('/search/:userId')
     async searchWithFavorites(
         @Param('userId') userId: number,
-        @Query('substr') substr: string,
-        @Query('skip') skip: number,
-        @Query('take') take: number,
-        @Query('order') order: 'asc' | 'desc',
-        @Query('sorting') sorting: keyof Omit<Song, 'id'> | 'favorite'
+        @Query('substr') substr?: string,
+        @Query('skip') skip?: string,
+        @Query('take') take?: string,
+        @Query('order') order?: 'asc' | 'desc',
+        @Query('sorting') sorting?: keyof Omit<Song, 'id'> | 'favorite'
     ) {
-        return this.songRepository.findAllWithFavorites(userId, { substr, skip, take, order, sorting });
+        return this.songRepository.findAllWithFavorites(userId, {
+            substr,
+            skip: skip ? parseInt(skip) : undefined,
+            take: take ? parseInt(take) : undefined,
+            order,
+            sorting,
+        });
     }
 
     @Get('/search')
     search(
-        @Query('substr') substr: string,
-        @Query('skip') skip: number,
-        @Query('take') take: number,
-        @Query('order') order: 'asc' | 'desc',
-        @Query('sorting') sorting: keyof Omit<Song, 'id'>
+        @Query('substr') substr?: string,
+        @Query('skip') skip?: string,
+        @Query('take') take?: string,
+        @Query('order') order?: 'asc' | 'desc',
+        @Query('sorting') sorting?: keyof Omit<Song, 'id'>
     ) {
-        return this.songRepository.findAll({ substr, skip, take, order, sorting });
+        return this.songRepository.findAll({
+            substr,
+            skip: skip ? parseInt(skip) : undefined,
+            take: take ? parseInt(take) : undefined,
+            order,
+            sorting,
+        });
     }
 }
